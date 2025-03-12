db59c49a9417762dece172202fda56bd1dc2b28e
EchoTheVee <taikorodon@gmail.com>
2025-03-12 10:19:17 -0700

    Got camera rotation working and basic player controller

---
+++ Assets/PlayerController/PlayerController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float moveSpeed;
+    public float jumpForce;
+    public float horizontalInput;
+    public float verticalInput;
+    public GameObject cam;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //float yRotation = cam.transform.eulerAngles.y;
+
+        Debug.Log($"cam rot y {cam.transform.rotation.y}");
+        transform.rotation = Quaternion.Euler(0, cam.transform.localEulerAngles.y, 0);
+        //transform.eulerAngles = new Vector3(transform.eulerAngles.x, yRotation, transform.eulerAngles.z);
+
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        rb.AddRelativeForce(Vector3.forward * moveSpeed * verticalInput);
+        rb.AddRelativeForce(Vector3.right * moveSpeed * horizontalInput);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+}
