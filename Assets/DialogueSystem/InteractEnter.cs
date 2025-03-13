d60deb3ad909b48b551145486c9d1b9ab2c41ae2
EchoTheVee <taikorodon@gmail.com>
2025-03-13 10:11:51 -0700

    Added Interactable Box for Dialogue

---
+++ Assets/DialogueSystem/InteractEnter.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class InteractEnter : MonoBehaviour
+{
+    public bool playerIsIn;
+    public bool playerHasInteracted;
+    public Dialogue d;
+    public GameObject dialogueBox;
+    public GameObject interactIcon;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerIsIn)
+        {
+            interactIcon.SetActive(true);
+        }
+        else
+        {
+            interactIcon.SetActive(false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.E) && playerIsIn == true && !playerHasInteracted)
+        {
+            playerHasInteracted = true;
+            dialogueBox.SetActive(true);
+            d.textComponent.text = string.Empty;
+            d.StartDialogue();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerIsIn = true;
+            //playerHasInteracted = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerIsIn = false;
+            playerHasInteracted = false;
+        }
+    }
+}
