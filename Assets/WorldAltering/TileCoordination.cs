49ecafbb7cdf074451bcd0f834dbfff58c317fbe
EchoTheVee <taikorodon@gmail.com>
2025-03-17 09:14:03 -0700

    GOT PROTOTYPE WORKING!

--- Assets/WorldAltering/TileCoordination.cs
+++ Assets/WorldAltering/TileCoordination.cs
@@ -4,17 +4,34 @@ using UnityEngine;
 
 public class TileCoordination : MonoBehaviour
 {
+    public MeshRenderer mr;
+    public Material on;
+    public Material off;
+
     public bool isActive;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        mr = GetComponent<MeshRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isActive)
+        {
+            mr.material = on;
+        }
+
+        if (!isActive)
+        {
+            mr.material = off;
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        isActive = !isActive;
     }
 }
