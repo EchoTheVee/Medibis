2c6405a0dc4033de42ac6ee36b328781723771a6
EchoTheVee <taikorodon@gmail.com>
2025-03-27 09:37:16 -0700

    Finished most of tutorial

--- Assets/PlayerController/PlayerController.cs
+++ Assets/PlayerController/PlayerController.cs
@@ -12,6 +12,7 @@ public class PlayerController : MonoBehaviour
     public float verticalInput;
     public GameObject cam;
     public bool isOnGround;
+    public float gravityForce;
     
     // Start is called before the first frame update
     void Start()
@@ -29,6 +30,10 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (!isOnGround)
+        {
+            rb.AddForce(Vector3.down * gravityForce, ForceMode.Impulse);
+        }
         //float yRotation = cam.transform.eulerAngles.y;
 
         Debug.Log($"cam rot y {cam.transform.rotation.y}");
